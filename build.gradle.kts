d60f8b8a96387bb9b6ceb8f7125fc78304ec7690
pnavgire998@gmail.com <pnavgire998@gmail.com>
2023-01-01 07:37:47 +0530

    test: failing test case for AddSKill

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/com/demo/user/AddSkillTest.kt
@@ -0,0 +1,11 @@
+package com.demo.user
+
+import org.junit.jupiter.api.Test
+
+class AddSkillTest {
+
+    @Test
+    fun `user should be able to add a skill to self`() {
+        val usecase = AddSkill()
+    }
+}
